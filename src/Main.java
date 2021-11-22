611fa42e44651d3421c893ee02d49d406e18501e
Dominik Kamykowski <kamykowskid@gmail.com>
2021-11-22 08:45:15 +0100

    Implements SerialPort Class - Create Class and implements basic methods

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    Gui gui = new Gui();
+
+    public static void main (String[] args){
+        try{
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/SerialPortClass.java
@@ -0,0 +1,38 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+
+public class SerialPortClass {
+
+    private static final int numberOfSendValues = 3;
+
+    SerialPort port = SerialPort.getCommPort("COM6");
+
+    public SerialPortClass(int baudRate){
+        System.out.println("SerialPort BaudRate: "+baudRate);
+        try{
+            port.openPort();
+            port.setBaudRate(baudRate);
+            System.out.println("Port name: "+port.getSystemPortName());
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    private void sendDataToSTM(boolean diodeStmState,boolean diodeGreenState, boolean diodeRedState){
+        try{
+            byte[] dataToWrite = {1,1,numberOfSendValues,(byte) ledState(diodeStmState),(byte) ledState(diodeGreenState),
+                    (byte)ledState(diodeRedState)};
+            port.writeBytes(dataToWrite,dataToWrite.length);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private int ledState(boolean diodeState){
+        if(diodeState) return 11;
+        else return 10;
+    }
+}
