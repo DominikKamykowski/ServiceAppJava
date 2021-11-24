180127604c18b52cd7977814ae974757be1422be
Dominik Kamykowski <kamykowskid@gmail.com>
2021-11-24 09:09:06 +0100

    Remove Main class, Change GUI class to main class

--- src/Gui.java
+++ src/Gui.java
@@ -8,4 +8,23 @@ public class Gui {
     private JPasswordField passwordField;
     private JSpinner comSpinner;
     private JButton connectButton;
+    private JPanel panel1;
+
+    public static void main (String[] args){
+        try{
+            JFrame frame = new JFrame("Gui");
+            frame.setContentPane(new Gui().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setSize(300, 300);
+            frame.setVisible(true);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        
+
+    }
+
 }
